e3ab1bce8e51bddf21999a470ce4c536fec734a5
Duncan C <dcai5@uwo.ca>
2015-11-28 14:10:12 -0500

    Facebook integration

--- app/src/main/java/com/example/android/groceryrideshare/MainActivity.java
+++ app/src/main/java/com/example/android/groceryrideshare/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.android.groceryrideshare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -20,13 +23,17 @@ import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    final static String LOG_TAG = MainActivity.class.getSimpleName();
     CallbackManager callbackManager;
+    LoginButton loginButton;
+    private PrefUtil prefUtil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +48,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        prefUtil = new PrefUtil(this);
 
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("user_friends");
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        // loginButton.setReadPermissions("user_friends");
         // If using in a fragment
         // loginButton.setFragment(this);
         // Other app specific specialization
@@ -52,9 +60,9 @@ public class MainActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Toast toast = Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_SHORT);
-                toast.show();
-                /* make the API call */
+                prefUtil.saveAccessToken(loginResult.getAccessToken().getToken());
+
+                /* make the API call
                 new GraphRequest(
                         AccessToken.getCurrentAccessToken(),
                         "/me",
@@ -62,12 +70,9 @@ public class MainActivity extends AppCompatActivity {
                         HttpMethod.GET,
                         new GraphRequest.Callback() {
                             public void onCompleted(GraphResponse response) {
-                                /* handle the result */
-                                Toast toast2 = Toast.makeText(getApplicationContext(), "All ur info is mine!", Toast.LENGTH_SHORT);
-                                toast2.show();
                             }
                         }
-                ).executeAsync();
+                ).executeAsync();*/
                 // App code
             }
 
@@ -100,6 +105,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -124,11 +134,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-
+        deleteAccessToken();
+        Profile profile = Profile.getCurrentProfile();
         // Logs 'install' and 'app activate' App Events.
+        if (profile != null) {
+            Toast.makeText(this, "Welcome " + profile.getFirstName(), Toast.LENGTH_SHORT).show();
+        }
         AppEventsLogger.activateApp(this);
     }
 
+
+    private void deleteAccessToken() {
+        AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+
+                if (currentAccessToken == null){
+                    //User logged out
+                    prefUtil.clearToken();
+                }
+            }
+        };
+    }
     @Override
     protected void onPause() {
         super.onPause();
